a0a1594b8479a1de1a9ef549a42401375cc9722a
Preston Mueller <skierjunk@gmail.com>
2017-10-05 20:44:07 -0400

    Update bootstrap.sh with osx apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo Updating and upgrading homebrew...
+brew update
+brew upgrade
+brew tap caskroom/drivers
+
+echo Installing dev stuff...
 brew install bash-completion git htop openssl readline carthage python3 android-sdk apktool
 
+echo Installing user stuff...
+brew cask install skype spotify sublime-text google-chrome firefox iterm2 tunnelblick keepingyouawake skim flux macdown spectacle 0xed dropbox 1password
+
+echo Pulling down git-aware-prompt and things...
 mkdir ~/.bash
 pushd ~/.bash
 
