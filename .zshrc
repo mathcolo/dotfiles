381fda28ec6f48f4272537abbcf59703b1774345
Preston Mueller <skierjunk@gmail.com>
2018-04-07 12:30:10 -0400

    Use zsh and add zprezto configs

---
+++ .zshrc
@@ -0,0 +1,6 @@
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,12 @@ brew upgrade
 brew tap caskroom/drivers
 
 echo Installing dev stuff...
-brew install bash-completion git htop openssl readline carthage python3 android-sdk apktool
+brew install zsh zsh-completions git htop openssl readline carthage python3 android-sdk apktool
+
+echo Updating login shell to zsh
+sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
+chsh -s /usr/local/bin/zsh
 
 echo Installing user stuff...
 brew cask install skype spotify sublime-text google-chrome firefox iterm2 tunnelblick keepingyouawake skim flux macdown spectacle 0xed dropbox 1password db-browser-for-sqlite
 
-echo Pulling down git-aware-prompt and things...
-mkdir ~/.bash
-pushd ~/.bash
-
-if [ ! -d "git-aware-prompt" ]; then
-  git clone git://github.com/jimeh/git-aware-prompt.git
-fi
-
-popd
