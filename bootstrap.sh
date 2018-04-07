c75834a54bf90813b9f9f1591bb18a62c9324e23
Preston Mueller <skierjunk@gmail.com>
2018-04-07 12:33:21 -0400

    Add vscode and pycharm community

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,5 +13,5 @@ sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
 echo Installing user stuff...
-brew cask install skype spotify sublime-text google-chrome firefox iterm2 tunnelblick keepingyouawake skim flux macdown spectacle 0xed dropbox 1password db-browser-for-sqlite
+brew cask install pycharm-ce visual-studio-code skype spotify sublime-text google-chrome firefox iterm2 tunnelblick keepingyouawake skim flux macdown spectacle 0xed dropbox 1password db-browser-for-sqlite
 
